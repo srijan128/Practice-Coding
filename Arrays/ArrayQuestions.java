b3952e2d096ad9c11ca47a4d84b75286c956cc6c
Srijan <srijan1290@gmail.com>
2024-09-29 06:54:57 +0530

    Array Questions

--- Arrays/ArrayQuestions.java
+++ Arrays/ArrayQuestions.java
@@ -3,10 +3,10 @@ package Arrays;
 import java.util.*;
 
 
-//Important Array Questions
+
 public class ArrayQuestions {
     public static void main(String[] args) {
-     //   removeDuplicatesFromASortedArray(new int[] {1,1,2,2,2,3,3});
+        removeDuplicatesFromASortedArray(new int[] {1,1,2,2,2,3,3});
         //setZeroes(new int[][]{{1,1,1},{1,0,1},{1,1,1}});
         rotateMatrix(new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}});
         System.out.println(longestSubarrayWithSumK(new int[]{1,2,3,1,1,1,1,4,2,3},3));
